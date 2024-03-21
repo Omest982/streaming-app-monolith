ab08f412796ad7c89e88b8c3a4eaea1dba68b8fb
Omest982 <93486447+Omest982@users.noreply.github.com>
2024-03-21 16:07:48 +0200

    Removed .env file from tracing.

---
+++ src/main/java/org/example/StreamingAppApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingAppApplication.class, args);
+    }
+}
